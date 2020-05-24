afcc01e3e258eae62c0e92d4a1cc5362e7093936
Styllerr <cork@ukr.net>
2020-05-24 13:13:14 +0300

    hw1

---
+++ ClassWork01/index.js
@@ -0,0 +1,2 @@
+import sumfn from 'lib';
+const result = samfn(2, 3);
---
+++ HomeWork_01/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      <h2 style={{color: 'green'}}>Lorem</h2>
+        <span style={{fontStyle:'italic'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
+         officia delectus, fuga vero rem quia maiores quidem minus nisi 
+         ducimus voluptatum ipsam deleniti odit autem</span>
+      </div>
+    )
+  }
+}
