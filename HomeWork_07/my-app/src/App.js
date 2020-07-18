fabd03b133b7cb68508131ac13caf4b1a6c21c50
Styllerr <cork@ukr.net>
2020-07-18 16:26:34 +0300

    create path

--- HomeWork_07/my-app/src/App.js
+++ HomeWork_07/my-app/src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
 import Container from '@material-ui/core/Container';
-import Navigation from './store/components/common/Navigation';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Navigation from './components/common/Navigation';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Users from './components/users/Users';
+import Alboms from './components/users/Alboms';
 
 function App() {
   return (
-    <Container maxWidth="lg">
-      <Navigation/>
-    </Container>
+    <Router>
+      <Container maxWidth="lg">
+        <Navigation />
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Switch>
+              <Route path='/' exact>
+                <Users />
+              </Route>
+              <Route path='/alboms'>
+                <Alboms />
+              </Route>
+              <Route path='*'>
+                <Redirect to='/' />
+              </Route>
+            </Switch>
+          </Grid>
+        </Grid>
+      </Container>
+    </Router>
   );
 }
 
